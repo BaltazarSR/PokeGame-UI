1b65d5a734c824932c40c184641d3c3339f4c5a4
Baltazar Servin <thebaltazarsr@gmail.com>
2025-04-02 02:19:40 -0600

    CSS changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,52 +4,74 @@ function App() {
 
   return (
     <>
-      <div style={{ display: 'flex', justifyContent: 'center'}}>
-        {/* Container game */}
-        <div style = {{ width: '350px', height: '500px', border: "1px solid black", borderRadius: "5px 5px 35px 5px"}}>
-          {/* Container screen */}
-          <div style={{paddingTop: '5%', paddingBottom: '25%', justifyContent: 'center', display: 'flex'}}>
-            <div style={{width: '85%', height: '200px', backgroundColor: 'olive'}}>
-
-            </div>
-          </div>
-          {/* Container buttons */}
-          <div style={{ display: 'flex', justifyContent: 'space-around'}}>
-            {/* First button */}
-            <div style={{ width: '60px', height: '60px', backgroundColor: 'black'}}>
-              <div>
-                <button
-                  style={{
-                    backgroundColor: 'blue',
-                    width: '40px',
-                    height: '40px',
-                  }}
-                ></button>
-              </div>
-            </div>
-            {/* Buttons Select Start */}
-            <div style={{ paddingTop: '30%'}}>
-              <div style={{ width: '60px', height: '60px', backgroundColor: 'gray'}}>
-                <div>
-                  <button style={{backgroundColor: '#A7A7A7', width: '40px', height: '10px', borderRadius: '25%'}}>
-                  </button>
+      <div class = "container-main">
+        {/* Shadow */}
+        <div class = "shadow">
+          {/* Container top */}
+          <div class = "container-top"></div>
+          {/* Container game */}
+          <div class = "container-gameboy">
+            {/* Container screen */}
+            <div class = "container-screen-center">
+              <div class = "container-screen">
+                {/* Container top screen */}
+                <div class = "container-screen-title">
+                  <div class = "container-dash-title1"></div>
+                  <div class = "container-dash-title2"></div>
                 </div>
-                <div style={{ paddingLeft: '90%'}}>
-                  <button style={{backgroundColor: '#A7A7A7', width: '40px', height: '10px', borderRadius: '25%'}}>
-                  </button>
-                </div>
+                {/* Container display */}
+                <div class = "container-display"></div>
+              </div>
+              {/* Container Nintendo */}
+              <div class = "container-name">
+              Nintendo GAMEBOY
               </div>
             </div>
-            {/* Buttons A B */}
-            <div style={{ width: '60px', height: '60px', backgroundColor: 'black', display: 'flex'}}>
-              {/* Button B */}
-              <div style={{ paddingTop: '50%'}}>
-                <button style={{backgroundColor: '#821660', width: '40px', height: '40px', borderRadius: '50%'}}>
-                </button>
-              </div>
-              <div>
-                <button style={{backgroundColor: '#821660', width: '40px', height: '40px', borderRadius: '50%'}}>
-                </button>
+            {/* Container buttons */}
+            <div class = "container-buttons-center">
+              <div class = "container-buttons">
+                {/* D-PAD */}
+                <div class = "container-dpad">
+                  <div class = "button-dpad-empty"></div>
+                  <div>
+                    <button class = "button-dpad"></button>
+                  </div>
+                  <div class = "button-dpad-empty"></div>
+                  <div>
+                    <button class = "button-dpad"></button>
+                  </div>
+                  <div>
+                    <button class = "button-dpad"></button>
+                  </div>
+                  <div>
+                    <button class = "button-dpad"></button>
+                  </div>
+                  <div class = "button-dpad-empty"></div>
+                  <div>
+                    <button class = "button-dpad"></button>
+                  </div>
+                  <div class = "button-dpad-empty"></div>
+                </div>
+                {/* SELECT START */}
+                <div style={{ paddingTop: '30%'}}>
+                  <div class = "container-ss">
+                    <button class = "button-ss"></button>
+                    <button class = "button-ss"></button>
+                  </div>
+                  <div class = "container-text-ss">
+                    <div class = "text-ss">SELECT</div>
+                    <div class = "text-ss">START</div>
+                  </div>
+                </div>
+                {/* A B */}
+                <div class = "container-ab">
+                  <div style={{ paddingTop: '30%'}}>
+                    <button class = "button-ab"></button>
+                  </div>
+                  <div style={{ paddingBottom: '30%'}}>
+                    <button class = "button-ab"></button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
